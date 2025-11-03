3d81975e3db3d85359bd881f80b73feebc948eba
Ángel de Jesús Fiscal Málaga <malagixcorp@hotmail.com>
2025-11-03 13:24:14 -0600

    feat: Estructura base con clases RSA y AES implementadas

---
+++ src/main/java/cifrador/Main.java
@@ -0,0 +1,13 @@
+package cifrador; 
+ 
+/** 
+ * Clase principal del Cifrador H¡brido RSA-AES 
+ * Proyecto de Criptograf¡a - Grupo 910B 
+ * Integrantes: µngel M laga, Luis Aguilar, Roberto Toto 
+ */ 
+public class Main { 
+    public static void main(String[] args) { 
+        System.out.println("=== CIFRADOR HÖBRIDO RSA-AES ==="); 
+        System.out.println("Iniciando desarrollo..."); 
+    } 
+} 
