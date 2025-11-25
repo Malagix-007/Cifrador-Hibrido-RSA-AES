4927f4cf967f023a670aa8c3dfb0ec8a131370b6
Angel Fiscal Malaga <malagixcorp@hotmail.com>
2025-11-24 21:37:33 -0600

    Update crear_jar.bat

--- crear_jar.bat
+++ crear_jar.bat
@@ -6,7 +6,6 @@ echo ---------------------------------------
 
 if not exist "out" mkdir "out"
 
-:: Compilamos referenciando tu carpeta 'javafx/lib' y 'libs'
 javac --module-path "javafx/lib" --add-modules=javafx.controls,javafx.fxml ^
  -cp "libs/mysql-connector-j-9.5.0.jar;." -d "out" ^
  "src/com/cifrador/utils/AESUtil.java" ^
@@ -16,7 +15,7 @@ javac --module-path "javafx/lib" --add-modules=javafx.controls,javafx.fxml ^
  "src/com/cifrador/ui/MainApp.java"
 
 if %errorlevel% neq 0 (
-  echo ❌ Error en compilacion. Revisa tu codigo.
+  echo Error en compilacion. Revisa tu codigo.
   pause
   exit /b
 )
@@ -25,15 +24,14 @@ echo.
 echo ---------------------------------------
 echo 2. Creando MANIFEST y JAR...
 echo ---------------------------------------
-:: Apuntamos al jar de mysql en la carpeta libs/
 echo Main-Class: com.cifrador.ui.MainApp > manifest.txt
 echo Class-Path: libs/mysql-connector-j-9.5.0.jar >> manifest.txt
 
-:: Creamos el jar en la raiz
 jar cvfm CifradorApp.jar manifest.txt -C out .
 
 del manifest.txt
 echo.
-echo ✅ EXITO: CifradorApp.jar creado en la raiz.
+echo EXITO: CifradorApp.jar creado en la raiz.
 echo Ahora abre Launch4j.
+
 pause
